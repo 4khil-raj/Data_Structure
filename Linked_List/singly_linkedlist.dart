e26e2565696781a54626da74f56b6f6def51bef3
Akhil Raj <akhilraj20801@gmail.com>
2024-01-15 19:31:45 +0530

    singlyco

--- Linked_List/singly_linkedlist.dart
+++ Linked_List/singly_linkedlist.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_initializing_formals
+
 class Node {
   int? data;
   Node? next;
@@ -10,4 +12,74 @@ class Node {
 class SinglyLinkedList {
   Node? head;
   Node? tail;
+
+//add value
+  void addNode(int data) {
+    Node newNode = Node(data);
+
+    if (head == null) {
+      head = newNode;
+    } else {
+      tail!.next = newNode;
+    }
+    tail = newNode;
+  }
+
+  //delete a element
+  void delete(int data) {
+    Node? temp = head;
+    Node? prev;
+    if (temp != null && temp.data == data) {
+      head = temp.next;
+    } else {
+      while (temp != null && temp.data != data) {
+        prev = temp;
+        temp = temp.next;
+      }
+      if (temp == null) {
+        return;
+      }
+      if (temp == tail) {
+        tail = prev;
+        tail?.next = null;
+        return;
+      }
+      prev?.next = temp.next;
+    }
+  }
+
+// to display the list value
+  void disply() {
+    Node? temp = head;
+    if (temp == null) {
+      print('node was emplty');
+    } else {
+      while (temp != null) {
+        print(temp.data);
+        temp = temp.next;
+      }
+    }
+  }
+
+  void insert(int after, int value) {
+    Node newnode = Node(value);
+    Node? temp = head;
+
+    while (temp != null && temp.data != after) {
+      temp = temp.next;
+    }
+    if (temp == null) {
+      return;
+    }
+  }
+}
+
+void main() {
+  SinglyLinkedList list = SinglyLinkedList();
+  list.addNode(10);
+  list.addNode(20);
+  list.addNode(50);
+  list.addNode(23);
+  list.delete(50);
+  list.disply();
 }
--- temp/temp.dart
+++ temp/temp.dart
@@ -1,12 +1,67 @@
-class Node {
+class Node1 {
   int? data;
-  Node? next;
-  Node(int data) {
-    this.data;
+  Node1? next;
+
+  Node1(int value) {
+    data = value;
   }
 }
 
-class SinglyLinkedlist {
-  Node? head;
-  Node? tail;
+class Slinkedlist {
+  Node1? head;
+  Node1? tail;
+
+  void add(int addvalue) {
+    Node1 newnodee = Node1(addvalue);
+    if (head == null) {
+      head = newnodee;
+    } else {
+      tail?.next = newnodee;
+    }
+    tail = newnodee;
+  }
+
+  void display() {
+    Node1? temp = head;
+
+    if (temp == null) {
+      return;
+    } else {
+      while (temp != null) {
+        print(temp.data);
+        temp = temp.next;
+      }
+    }
+  }
+
+  void delete(int value) {
+    Node1? temp = head;
+    Node1? prev;
+    if (temp != null && temp.data == value) {
+      head = temp.next;
+    } else {
+      while (temp != null && temp.data != value) {
+        prev = temp;
+        temp = temp.next;
+      }
+      if (temp == null) {
+        return;
+      }
+      if (temp == tail) {
+        tail = prev;
+        tail?.next = null;
+      }
+      prev?.next = temp.next;
+    }
+  }
+}
+
+void main() {
+  Slinkedlist list = Slinkedlist();
+  list.add(100);
+  list.add(200);
+  list.add(300);
+  list.add(400);
+  list.delete(100);
+  list.display();
 }
