b6c1f94c93dd708bb1e4074b18cb9b0a4e8ab225
Akhil Raj <akhilraj20801@gmail.com>
2024-01-18 22:10:45 +0530

    before review

---
+++ Recursion/sum.dart
@@ -0,0 +1,11 @@
+int sum(int n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    return n + sum(n - 1);
+  }
+}
+
+void main() {
+  return print(sum(3));
+}
--- temp/temp.dart
+++ temp/temp.dart
@@ -1,11 +1,27 @@
-import 'dart:io';
+int binary(List<int> list, int target) {
+  int low = 0;
+  int high = list.length - 1;
+  while (low <= high) {
+    int mid = (low + high) ~/ 2;
+    int midvalue = list[mid];
+    if (midvalue == target) {
+      return mid;
+    } else if (midvalue < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
 
 void main() {
-  String s = 'AAABBC';
-  int count = 0;
-  for (var i = 0; i < s.length; i++) {
-    if (s.contains('A')) {
-      count++;
-    }
+  List<int> list = [1, 2, 3, 4, 5, 6, 7, 8];
+  int target = 5;
+  int result = binary(list, target);
+  if (result == -1) {
+    print('target is not in the list');
+  } else {
+    print('value found index$result');
   }
 }
