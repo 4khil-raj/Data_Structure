30b559b2b438c8651d3e89c6e7dd47e60ef3d7ab
Akhil Raj <akhilraj20801@gmail.com>
2024-02-02 18:40:52 +0530

    2nd week end

---
+++ HashMap/hashmap.dart
@@ -0,0 +1,12 @@
+import 'dart:collection';
+
+void main() {
+  HashMap<String, int> studentage = HashMap<String, int>();
+
+  studentage['a'] = 20;
+  studentage['b'] = 30;
+  studentage['c'] = 40;
+  studentage['d'] = 50;
+
+  print('student age is${studentage['a']}');
+}
