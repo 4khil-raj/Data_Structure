078044612cab68d984c484f410cf377aac5e0c65
Akhil Raj <akhilraj20801@gmail.com>
2024-03-28 08:57:39 +0530

    full domain review

--- temp/temp.dart
+++ temp/temp.dart
@@ -1,17 +1,17 @@
-void main() {
-  List<int> list = [5, 2, 7, 6, 99, 3, 1, 4, 25, 55, 10];
-  print(bubble(list));
-}
+import 'dart:collection';
 
-List bubble(List<int> list) {
-  for (int i = 0; i < list.length - 1; i++) {
-    for (int j = 0; j < list.length - 1 - i; j++) {
-      if (list[j] > list[j + 1]) {
-        int temp = list[j];
-        list[j] = list[j + 1];
-        list[j + 1] = temp;
-      }
+class Graphrevice {
+  HashMap<int, List<int>> graph = HashMap();
+  void insert(int vertex, int edge, bool isBidirection) {
+    if (!graph.containsKey(vertex)) {
+      graph[vertex] = [];
+    }
+    if (!graph.containsKey(edge)) {
+      graph[edge] = [];
+    }
+    graph[vertex]!.add(edge);
+    if (isBidirection) {
+      graph[vertex]!.add(edge);
     }
   }
-  return list;
 }
